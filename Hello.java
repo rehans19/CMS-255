3bf7da2e957c571e0d534fdc7e12d21e20669ac8
rehans19 <59926537+rehans19@users.noreply.github.com>
2021-01-27 21:30:50 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,21 @@
+/*****************************************
+Hello Java
+******************************************
+* Sarina Rehan
+*January 27, 2021
+* CMS 255-003
+*****************************************/ 
+
+public class Hello {
+
+	public static void main (String[] args) {
+
+		System.out.println("Sarina Rehan");
+		System.out.println("Hello Java");
+		System.out.println("January 25, 2021"); 
+		System.out.println("CMS 255-003");
+		System.out.println();
+		
+
+	}
+}
